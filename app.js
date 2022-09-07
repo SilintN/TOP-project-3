1004bac719790549f18aa2b7f38f80ebf6c22fe4
silint <silintnight23@gmail.com>
2022-09-07 10:32:55 -0700

    Got first function to work
    First function is to make the computer randomly choose and option from the array.

---
+++ app.js
@@ -0,0 +1,11 @@
+//Game
+
+let choices = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+getComputerChoice();
+
+function playRound() {}
+playRound();
