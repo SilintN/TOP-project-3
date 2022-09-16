cea17fa20047491ec52a1b969c50d356a3e8eab6
silint <silintnight23@gmail.com>
2022-09-16 07:24:41 -0700

    Redoing my RPS project
    Updated README with all of assignment instructions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//Game
+//Game - first try
 
-const choices = ['Rock', 'Paper', 'Scissors'];
+/* let choices = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -24,3 +24,44 @@ playRound();
 console.log(playRound());
 
 function game() {}
+*/
+
+// Game - second try
+
+// compChoice is the first variable used to get the computers choice randomly using the Math.random method
+// let compChoice = getComputerChoice();
+// let playerChoice = prompt('Choose Rock, Paper, or Scissors!');
+
+function getComputerChoice() {
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      console.log('Rock');
+      break;
+    case 1:
+      console.log('Paper');
+      break;
+    case 2:
+      console.log('Scissors');
+      break;
+    default:
+      console.log('Something is Wrong');
+  }
+}
+
+getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log('Tie!');
+  } else if (playerSelection > computerSelection) {
+    console.log('You win!');
+  } else if (playerSelection < computerSelection) {
+    console.log('You lose!');
+  } else {
+    console.log('Something Broke!');
+  }
+}
+
+playRound();
+
+// console.log(playRound());
